99b081b5a2c21fba31f7ac1e6a88fc2bc9f96d9d
johndesu090 <37970753+johndesu090@users.noreply.github.com>
2020-03-16 18:31:29 +0800

    Add files via upload

---
+++ misc/recruitment.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+. $HOME/helpers/support/string-helper.sh
+
+echo "";
+info "JohnFordTV is looking for contributors and donators."
+info "GCASH: 09206200840 PAYPAL: johnford090@gmail.com"
+info "Contact me in Facebool :"
+danger "https://www.facebook.com/johndesu090"
+success "Thanks!"
+echo "-------------"
